92b475f312f450207e15808526bd099c06bd6310
Bruno Carlos <6951456+brnovasco@users.noreply.github.com>
2024-06-23 16:35:10 -0300

    add toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import {
 } from "@/components/ui/select";
 import { GAMES, type GameName } from "@/lib/constants";
 import { Textarea } from "@/components/ui/textarea";
+import { toast } from "sonner"
 
 export default function Home() {
   const [numbers, setNumbers] = useState<number[] | undefined>();
@@ -25,6 +26,7 @@ export default function Home() {
     if (!numbers) return;
     if (!gameName) return;
     if (!phrase) return;
+    toast.info("Salvando...");
     try {
       await db.games.add({
         gameName,
@@ -33,9 +35,10 @@ export default function Home() {
         phrase,
         createdAt: new Date(),
       });
-      console.log("Saved to db");
+      toast.success("Salvo com sucesso!");
     } catch (error) {
       console.error(error);
+      toast.error("Erro ao salvar!");
     }
   }
 
